6b11fde1d60a2615b46e80f674a2226f5e0a3097
Nick Tramper <nick.tramper@gmail.com>
2021-10-18 21:56:32 +0200

    Configurations are buildable images

--- configurationManager/containerManager.go
+++ configurationManager/containerManager.go
@@ -1,63 +1,40 @@
 package configurationManager
 
 import (
-	"archive/tar"
-	"bytes"
 	"context"
 	"github.com/docker/docker/api/types"
 	"github.com/docker/docker/api/types/container"
-	"io"
+	"github.com/docker/docker/pkg/archive"
 	"log"
-	"os"
 )
+
+var configurationsFolder = "./configurations/"
 
 func (c *Configuration) BuildContainer(cm ConfigurationManager) {
 	ctx := context.Background()
 
-	buf := new(bytes.Buffer)
-	tw := tar.NewWriter(buf)
-	defer tw.Close()
-
-	readDockerFile, err := cm.Box.Find(c.Path)
-	if err != nil {
-		log.Println(err, " :unable to read Dockerfile")
-	}
-
-	tarHeader := &tar.Header{
-		Name: DockerPrefix + c.Name,
-		Size: int64(len(readDockerFile)),
-	}
-	err = tw.WriteHeader(tarHeader)
-	if err != nil {
-		log.Println(err, " :unable to write tar header")
-	}
-	_, err = tw.Write(readDockerFile)
+	containerDir, err := archive.TarWithOptions(configurationsFolder + c.Name, &archive.TarOptions{})
 	if err != nil {
 		log.Println(err, " :unable to write tar body")
 	}
-	dockerFileTarReader := bytes.NewReader(buf.Bytes())
 
 	imageBuildResponse, err := cm.Client.ImageBuild(
 		ctx,
-		dockerFileTarReader,
+		containerDir,
 		types.ImageBuildOptions{
-			Context:    dockerFileTarReader,
-			Dockerfile: DockerPrefix + c.Name,
+			Dockerfile: "Dockerfile",
 			Remove:     true,
+			Tags: []string{DockerPrefix + c.Name},
 		},
 	)
-	if err != nil {
-		log.Println(err, " :unable to build docker image")
-	}
 	defer imageBuildResponse.Body.Close()
-	_, err = io.Copy(os.Stdout, imageBuildResponse.Body)
-	if err != nil {
-		log.Fatal(err, " :unable to read image build response")
-	}
+	//_, err = io.Copy(os.Stdout, imageBuildResponse.Body)
+	//if err != nil {
+	//	log.Println(err, " :unable to read image build response")
+	//}
+	//log.Println(imageBuildResponse.Body)
 
-	log.Println("build image")
-	log.Println(imageBuildResponse.Body)
-	//c.CreateContainer(cm)
+	c.CreateContainer(cm)
 }
 
 func (c *Configuration) CreateContainer(cm ConfigurationManager) {
